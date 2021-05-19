dad4ffa879e9ebc4bd8fe2c2799e979484d59b55
wc356 <wc0599@princeton.edu>
2021-05-19 16:24:46 -0400

    add & format external links

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,21 +1,48 @@
 import { Link } from "react-router-dom";
+import "./HomeView.scss";
 
-const HomeView = () => (
-  <>
-    <h1>AgGrid Basics in React</h1>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/aggrid-basics">AgGrid Basics</Link>
-        </li>
-        <li>
-          <Link to="/aggrid-update-delete-columns">
-            Updating and Deleting Columns
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </>
-);
+const HomeView = () => {
+  const renderExternalLink = (href, text) => (
+    <li>
+      <a href={href} referrerPolicy="no-referrer" target="_blank">
+        {text}
+      </a>
+    </li>
+  );
+
+  const renderLink = (url, text) => (
+    <li>
+      <Link to={url}>{text}</Link>
+    </li>
+  );
+
+  return (
+    <>
+      <h1>AgGrid Basics in React</h1>
+      <nav>
+        <ol>
+          {renderLink("/aggrid-basics", "Ag Grid Basics")}
+          {renderLink(
+            "/aggrid-update-delete-columns",
+            "Updating and Deleting Columns"
+          )}
+        </ol>
+      </nav>
+      <nav>
+        <p>External Links:</p>
+        <ul>
+          {renderExternalLink(
+            "https://github.com/wc0599/AgGrid-React",
+            "GitHub"
+          )}
+          {renderExternalLink(
+            "https://www.ag-grid.com/react-grid/",
+            "Ag-Grid Docs"
+          )}
+        </ul>
+      </nav>
+    </>
+  );
+};
 
 export default HomeView;
